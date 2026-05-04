add4b358aa16cdd0a90bdb596e40e8c6f2d1b3e3
muhammadusam <muhammadusman999999999@gmail.com>
2026-05-04 23:52:26 +0500

    Hajj Operations Page setup but not finish the table

--- components/Navbar.js
+++ components/Navbar.js
@@ -39,6 +39,7 @@ const Navbar = () => {
                   <ul className="absolute left-0 mt-1 w-40 bg-black text-white rounded shadow-md opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
                     <Link legacyBehavior href="/secp/hajj2024"><li className="px-4 py-2 rounded hover:bg-gray-700"><a href="#">Hajj 2024</a></li></Link>
                     <Link legacyBehavior href="/secp/hajj2025"><li className="px-4 py-2 rounded hover:bg-gray-700"><a>Hajj 2025</a></li></Link>
+                    <Link legacyBehavior href="/hajj-operations/hajj-2026"><li className="px-4 py-2 rounded hover:bg-gray-700"><a>Hajj 2026</a></li></Link>
                     {/* <li className="px-4 py-2 hover:bg-gray-700"><a href="#">Marketing</a></li> */}
                   </ul>
                 </div>
